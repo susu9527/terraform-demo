7aa5443ae2bc2687646ff5c6b62f0cca91081acf
susu9527 <73459029+susu9527@users.noreply.github.com>
2021-01-19 14:08:51 +0800

    Update test.tf

--- com/aliyun/demo/test.tf
+++ com/aliyun/demo/test.tf
@@ -3,13 +3,4 @@
 provider "alicloud" {
   region = "cn-hangzhou"
 }
-data "alicloud_vpcs" "default" {
-  is_default = true
-}
 
-
-resource "alicloud_security_group" "demo" {
-  count = 4
-  name = "cloud-demo-test-${count.index}"
-  vpc_id = data.alicloud_vpcs.default.ids.0
-}
